5950182e84a423a5ed83b8e89da2a24a2c93f9af
redstoone <cy_jing_2010@163.com>
2018-02-22 17:05:19 +0800

    userInfo完善

--- client/src/panel/UserInfo.ts
+++ client/src/panel/UserInfo.ts
@@ -5,11 +5,12 @@
  */
 
 class UserInfo extends BaseComponent {
-
 	private static instance: UserInfo;
 	private userAvatar: eui.Image;
 	private userName: eui.Label;
 	private userId: eui.Label;
+	private goldNum: eui.Label;
+	private goldAddBtn: eui.Button;
 
 	public constructor() {
 		super();
@@ -18,8 +19,16 @@ class UserInfo extends BaseComponent {
 		this.load("component/UserInfoSkin.exml");
 
 		var inst = UserInfo.instance;
-		inst.x = 32;
-		inst.y = 10;
+		inst.x = 0;
+		inst.y = 0;
 		Main.getInstance().addChild(inst);
+	}
+
+	protected initComponent() {
+		this.goldAddBtn.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onGoldAddBtnTouch, this)
+	}
+
+	private onGoldAddBtnTouch(ev: egret.TouchEvent): void {
+		console.log('gold add:', ev, this)
 	}
 }
